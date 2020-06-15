5e181a260055bce209bbb5061ea6b14a7537e73d
Nouha Mbarek <nouha.mbarek@CHIFCO.PROD>
2020-06-15 17:51:34 +0200

    dev

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Puzzle from 'react-image-puzzle';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
-
+//https://upload.wikimedia.org/wikipedia/fr/8/87/Disney_Channel_2014.png
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
